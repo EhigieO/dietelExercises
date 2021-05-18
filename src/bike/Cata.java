0b7162cc9b5985f9c65437e85b2917419ad49b6c
EhigieO <81506985+EhigieO@users.noreply.github.com>
2021-05-18 08:04:20 +0100

    added cata number Palindrome

---
+++ src/bike/Cata.java
@@ -0,0 +1,27 @@
+package bike;
+import java.util.Scanner;
+public class C909ata
+{
+    public static void main(String... args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int number;
+        int reversed = 0;
+        System.out.println("Enter number: ");
+        number = input.nextInt();
+        int pal = number + 0;
+
+        while(number != 0)
+        {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        System.out.println(reversed);
+        if (reversed == pal) {
+            System.out.print("Number is a Palindrome " + reversed);
+        }
+
+    }
+}
