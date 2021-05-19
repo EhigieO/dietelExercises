2344221e6872b8f8b915add7a27472e4d1efccc5
EhigieO <81506985+EhigieO@users.noreply.github.com>
2021-05-19 17:10:35 +0100

    added the else if version of school grade

---
+++ src/bike/NewSchoolGrader.java
@@ -0,0 +1,27 @@
+package bike;
+
+import java.util.Scanner;
+
+public class NewSchoolGrader {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Student Grade: ");
+        int grade;
+        grade = input.nextInt();
+        if (grade > 89) {
+            System.out.println("Excellent A");
+        }else
+        if (grade > 79 && grade < 90) {
+            System.out.println("Great B");
+        }else
+        if (grade > 69 && grade < 80) {
+            System.out.println("Good C");
+        }else
+        if (grade > 59 && grade < 70) {
+            System.out.println("Pass D");
+        }
+        if (grade < 60) {
+            System.out.println("Failed F");
+        }
+    }
+}
