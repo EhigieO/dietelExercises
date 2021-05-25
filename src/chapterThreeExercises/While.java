b6274a16981ae7eb7ee5eadae2ca68154130e8ad
EhigieO <81506985+EhigieO@users.noreply.github.com>
2021-05-25 09:16:22 +0100

    added EvenNumbers class

---
+++ src/chapterThreeExercises/While.java
@@ -0,0 +1,11 @@
+package chapterThreeExercises;
+
+public class While {
+    public static void main (String... args) {
+        int number = 0;
+        while ( number < 25) {
+            System.out.println("Hello world");
+            number = number + 5;
+        }
+    }
+}
---
+++ test/CataTest.java
@@ -0,0 +1,18 @@
+import chapterThreeExercises.Cata;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.AssertEquals.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CataTest {
+    private Cata cata;
+    @BeforeEach
+    void each_Start_With(){
+        cata = new Cata();
+    }
+    @Test
+    void test_That_() {
+        assertEquals();
+    }
+}
