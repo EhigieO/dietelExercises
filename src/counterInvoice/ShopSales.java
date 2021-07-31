c547ee23d42a6d03af89449d93a4720d82a46d5b
EhigieO <81506985+EhigieO@users.noreply.github.com>
2021-08-01 00:05:33 +0100

    added exception to receipt

--- src/counterInvoice/ShopSales.java
+++ src/counterInvoice/ShopSales.java
@@ -1,5 +1,6 @@
 package counterInvoice;
 
+import java.security.InvalidParameterException;
 import java.util.Scanner;
 
 public class ShopSales {
@@ -13,25 +14,41 @@ public class ShopSales {
         int proceed = 1;
         System.out.println("Your one stop shop");
         do {
+
             System.out.println("Enter Item name:");
             itemName = scanner.next();
+            try {
+                System.out.println("Enter the price per item:");
+                price = scanner.nextInt();
+                if (price < 1){
+                    throw new InvalidParameterException("Enter a valid price");
+                }
 
-            System.out.println("Enter the price per item:");
-            price = scanner.nextInt();
-
-            System.out.println("Enter number of items bought:");
-            quantity = scanner.nextInt();
-
-            Item item = new Item(itemName,price,quantity);
-            cart.addItem(item);
-            totalPrice += price*quantity;
-
+                System.out.println("Enter number of items bought:");
+                quantity = scanner.nextInt();
+                if (quantity < 1){
+                    throw new InvalidParameterException("Enter a valid quantity");
+                }
+                Item item = new Item(itemName, price, quantity);
+                cart.addItem(item);
+                totalPrice += price * quantity;
+            } catch (InvalidParameterException error) {
+                System.out.println(error.getMessage());
+            }
+            try {
             String message = """
                     1. For more purchase
                     2. To print receipt
                     """;
             System.out.println(message);
-            proceed = scanner.nextInt();
+
+                proceed = scanner.nextInt();
+                if (proceed != 1 && proceed != 2){
+                    throw new IllegalArgumentException("Select a valid option");
+                }
+            }catch (IllegalArgumentException error) {
+                System.out.println(error.getMessage());
+            }
         } while (proceed == 1);
         displayInvoice(cart);
         System.out.printf("%35.2f\t",totalPrice);
