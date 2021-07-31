bbc2f747a868fd843ab2079cbf3e7cbc2dc72d86
EhigieO <81506985+EhigieO@users.noreply.github.com>
2021-07-31 23:09:49 +0100

    the tdd of receipt generation

--- src/counterInvoice/ShopSales.java
+++ src/counterInvoice/ShopSales.java
@@ -1,16 +1,12 @@
 package counterInvoice;
 
-import assignmentStore.Receipt;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class ShopSales {
     public static void main(String[] args) {
         Cart cart = new Cart();
         Scanner scanner = new Scanner(System.in);
-        int totalQuantity = 0;
+        double totalPrice = 0;
         int quantity;
         int price;
         String itemName;
@@ -28,7 +24,7 @@ public class ShopSales {
 
             Item item = new Item(itemName,price,quantity);
             cart.addItem(item);
-            totalQuantity += price*quantity;
+            totalPrice += price*quantity;
 
             String message = """
                     1. For more purchase
@@ -37,16 +33,19 @@ public class ShopSales {
             System.out.println(message);
             proceed = scanner.nextInt();
         } while (proceed == 1);
-        displayInvoice();
+        displayInvoice(cart);
+        System.out.printf("%35.2f\t",totalPrice);
     }
 
     private static void displayInvoice(Cart itemCart) {
-        Cart items = itemCart;
-        System.out.printf("%30s\n", "RECEIPT");
-        System.out.println("Number of Items\tItem\tPrice\tQuantity\tTotal");
-        for (int i = 0; i < items.displayItems().size();i++) {
-            Item item = items.displayItems().get(i);
-            String itemName =
+        System.out.println("*******************RECEIPT*******************");
+        System.out.printf("%s %8s %10s %10s\n","Quantity","Item","Price","total");
+        for (int i = 0; i < itemCart.displayItems().size(); i++) {
+            Item item = itemCart.displayItems().get(i);
+            int quantity = item.getNumberOfItem();
+            String name = item.getName();
+            double price = item.getItemPrice();
+            System.out.printf("%d %8s %10.2f%n",quantity,name,price);
         }
     }
 }
